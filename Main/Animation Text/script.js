518628af215669c2ab1041a0d5c312854983f3d3
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2020-09-28 21:21:17 +0700

    layout

--- Main/Animation Text/script.js
+++ Main/Animation Text/script.js
@@ -55,3 +55,15 @@ window.addEventListener('scroll', function(){
   text.style.top = value * 1 + 'px';
   text.style.top = value * 1 + 'px';
 })
+
+document.documentElement.className = 'js';
+	var supportsCssVars = function() {
+	var s = document.createElement('style'),support;
+
+		s.innerHTML = "root: { --tmp-var: bold; }";
+		document.head.appendChild(s);
+		support = !!(window.CSS && window.CSS.supports && window.CSS.supports('font-weight', 'var(--tmp-var)'));
+		s.parentNode.removeChild(s);
+		return support;
+	}
+		if (!supportsCssVars()) alert('Please view this demo in a modern browser that supports CSS Variables.')
