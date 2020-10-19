8399e5042c6d8fc3999e7d78076ea8ccbf5e6cd4
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2020-10-19 22:40:53 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ gemData([
     },
     {
       "id": 1,
-      "name": "Talc ทัลก์",
+      "name": "Talc ทัลก์03",
       "formula": "Mg3Si4O10(OH)2",
       "type": "แร่ไฮเดรตแมกนิเซียมซิลิเกต",
       "color": "ขาว เทา หรือฟ้า",
@@ -19,20 +19,308 @@ gemData([
     },
     {
       "id": 2,
-      "name": "Talc ทัลก์02",
-      "formula": "Mg3Si4O10(OH)2",
-      "type": "แร่ไฮเดรตแมกนิเซียมซิลิเกต",
-      "color": "ขาว เทา หรือฟ้า",
-      "property": "แร่ทัลก์ เป็นแร่ที่มีความแข็ง 1 ตามสเกลของโมส์ ซึ่งอ่อนมากจนสามารถใช้เล็บขูดเป็นรอยได้ มีลักษณะผลึกเป็นแผ่นหนา มีรูปผลึกเป็นระบบโมโนคลินิก รูปสี่เหลี่ยมขนมเปียกปูนและหกเหลี่ยม",
-      "benefit": "ป่นเป็นผงเพื่อใช้ทำสี เครื่องปั้นดินเผา ยาฆ่าแมลง วัสดุมุงหลังคา วัสดุทนไฟ ใช้บุผนังแบบหล่อ ใช้ในอุตสาหกรรมกระดาษ ยาง พลาสติกและสิ่งทอ ใช้ในอุตสาหกรรมเครื่องสำอาง"
+      "name": "Gypsum ยิปซัม หรือเรียกว่าเกลือจืด",
+      "formula": "CaSO4·2H2O",
+      "type": "แร่อโลหะ",
+      "color": "ขาว เทา หรือชมพูแดง",
+      "property": "มีค่าความแข็งที่ 2 ตามสเกลของโมส์ (Moh's scale) ลักษณะที่พบโดยทั่วไป เป็นแบบเนื้อแน่น และมีรูปผลึกเฉพาะ",
+      "benefit": "สามารถนำมาทำปูนปลาสเตอร์ ปูนซีเมนต์ (portland cement) แผ่นยิปซัมอัด (gypsum board) หรือแผ่นยิปซัมบอร์ด ชอล์ก กระดาษ และปุ๋ย ส่วนแร่ชนิดซาตินสปาร์ และอะลาบาสเตอร์ อาจนำมาใช้ในการแกะสลักหรือขัดทำเป็นเครื่องประดับ"
     },
     {
       "id": 3,
-      "name": "Talc ทัลก์03",
-      "formula": "Mg3Si4O10(OH)2",
-      "type": "แร่ไฮเดรตแมกนิเซียมซิลิเกต",
-      "color": "ขาว เทา หรือฟ้า",
-      "property": "แร่ทัลก์ เป็นแร่ที่มีความแข็ง 1 ตามสเกลของโมส์ ซึ่งอ่อนมากจนสามารถใช้เล็บขูดเป็นรอยได้ มีลักษณะผลึกเป็นแผ่นหนา มีรูปผลึกเป็นระบบโมโนคลินิก รูปสี่เหลี่ยมขนมเปียกปูนและหกเหลี่ยม",
-      "benefit": "ป่นเป็นผงเพื่อใช้ทำสี เครื่องปั้นดินเผา ยาฆ่าแมลง วัสดุมุงหลังคา วัสดุทนไฟ ใช้บุผนังแบบหล่อ ใช้ในอุตสาหกรรมกระดาษ ยาง พลาสติกและสิ่งทอ ใช้ในอุตสาหกรรมเครื่องสำอาง"
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 4,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 5,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 6,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 7,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 8,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 9,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 10,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 11,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 12,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 13,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 14,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 15,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 16,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 17,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 18,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 19,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 20,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 21,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 22,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 23,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 24,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 25,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 26,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 27,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 28,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 29,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 30,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 31,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 32,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 33,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 34,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
+    },
+    {
+      "id": 35,
+      "name": "gun",
+      "formula": "gun",
+      "type": "gun",
+      "color": "gun",
+      "property": "gun",
+      "benefit": "gun"
     }
 ]);
